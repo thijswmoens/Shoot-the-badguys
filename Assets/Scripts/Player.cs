a1c7ef665dc56bbbda1e3e0c69b4cb0cdcc37b34
Thijs Moens <post@thijs.email>
2022-07-18 13:33:34 +0200

    Update with Ray

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,11 +6,14 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float moveSpeed = 5;
+
+    Camera viewCamera;
     PlayerController controller;
 
     void Start()
     {
         controller = GetComponent<PlayerController>();
+        viewCamera = Camera.main;
     }
 
     void Update()
@@ -18,5 +21,14 @@ public class Player : MonoBehaviour
         Vector3 moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
         Vector3 moveVelocity = moveInput.normalized * moveSpeed;
         controller.Move(moveVelocity);
+
+        Ray ray = viewCamera.ScreenPointToRay(Input.mousePosition);
+        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+        float rayDistance;
+
+        if(groundPlane.Raycast(ray, out rayDistance))
+        {
+            Vector3 point = ray.GetPoint(rayDistance);
+        }
     }
 }
